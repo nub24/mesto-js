05d62164194f6203c00208e05ce4e78b1017ea48
Алексей <32929527+nub24@users.noreply.github.com>
2022-12-28 19:47:48 +0700

    hover on mobile-buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,17 @@
 'use strict';
 
-let likeButtons = document.querySelectorAll('.like-button');
-
-
 // Реализация нажатия/отжатия лайка на карточке
+let likeButtons = document.querySelectorAll('.like-button');
+let likeButtonPath = './images/liked-button.svg';
+let unlikeButtonPath = './images/unliked-button.svg';
+
 for (let i = 0; i < likeButtons.length; i++) {
   likeButtons[i].setAttribute('src', './images/unliked-button.svg');
   likeButtons[i].addEventListener('click', function() {
-    if (likeButtons[i].getAttribute('src') === './images/unliked-button.svg') {
-      likeButtons[i].setAttribute('src', './images/liked-button.svg');
+    if (likeButtons[i].getAttribute('src') === unlikeButtonPath) {
+      likeButtons[i].setAttribute('src', likeButtonPath);
     } else {
-      likeButtons[i].setAttribute('src', './images/unliked-button.svg');
+      likeButtons[i].setAttribute('src', unlikeButtonPath);
     }
   });
 }
