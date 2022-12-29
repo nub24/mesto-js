f44b7069844d931bc3caae2a700e9e47e6b858a1
Алексей <32929527+nub24@users.noreply.github.com>
2022-12-29 17:16:37 +0700

    features

--- scripts/index.js
+++ scripts/index.js
@@ -16,6 +16,38 @@ for (let i = 0; i < likeButtons.length; i++) {
   });
 }
 
+//Переменные для формы редактирования
+let popupWindow = document.querySelector('.popup');
+let inputName = document.querySelector('.popup__name');
+let inputDescription = document.querySelector('.popup__description');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let editButton = document.querySelector('.edit-button');
+let submitButton = document.querySelector('.save-button');
+let closePopupButton = document.querySelector('.popup__close-icon');
 
+//Фкнкция открытия popup
+function popupOpen() {
+  //Значения инпутов при инициализвци popup-окна.
+  inputName.value = profileTitle.textContent;
+  inputDescription.value = profileSubtitle.textContent;
 
+  popupWindow.classList.replace('popup__unactive', 'popup__active');
+}
 
+//Фкнкция закрытия popup
+function popupClose() {
+  popupWindow.classList.replace('popup__active', 'popup__unactive');
+}
+
+//Функция отправки формы
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  profileTitle.textContent = inputName.value;
+  profileSubtitle.textContent = inputDescription.value;
+  popupWindow.classList.replace('popup__active', 'popup__unactive');
+}
+
+editButton.addEventListener('click', popupOpen);
+submitButton.addEventListener('click', handleFormSubmit);
+closePopupButton.addEventListener('click', popupClose)
