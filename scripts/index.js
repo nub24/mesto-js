6a9c9ff28ded7558f60787a3f2b0a11cee80ad15
Алексей <32929527+nub24@users.noreply.github.com>
2023-01-01 23:10:15 +0700

    adaptive + overflow profile

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,21 @@
 'use strict';
 
 // Реализация нажатия/отжатия лайка на карточке
-let likeButtons = document.querySelectorAll('.like-button');
-let likeButtonPath = './images/liked-button.svg';
-let unlikeButtonPath = './images/unliked-button.svg';
+// let likeButtons = document.querySelectorAll('.like-button');
+// let likeButtonPath = './images/liked-button.svg';
+// let unlikeButtonPath = './images/unliked-button.svg';
 
-for (let i = 0; i < likeButtons.length; i++) {
-  likeButtons[i].setAttribute('src', './images/unliked-button.svg');
-  likeButtons[i].addEventListener('click', function() {
-    if (likeButtons[i].getAttribute('src') === unlikeButtonPath) {
-      likeButtons[i].setAttribute('src', likeButtonPath);
-    } else {
-      likeButtons[i].setAttribute('src', unlikeButtonPath);
-    }
-  });
-}
+// for (let i = 0; i < likeButtons.length; i++) {
+//   likeButtons[i].setAttribute('src', './images/unliked-button.svg');
+//   likeButtons[i].addEventListener('click', function() {
+//     if (likeButtons[i].getAttribute('src') === unlikeButtonPath) {
+//       likeButtons[i].setAttribute('src', likeButtonPath);
+//     } else {
+//       likeButtons[i].setAttribute('src', unlikeButtonPath);
+//     }
+//   });
+// }
+
 
 //Переменные для формы редактирования
 let popupWindow = document.querySelector('.popup');
