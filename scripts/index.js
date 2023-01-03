501ee6bb59895545708f076b1f9111aefa489b1a
Алексей <32929527+nub24@users.noreply.github.com>
2023-01-04 01:07:35 +0700

    iteration 1 fixes

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,13 @@
 
 //Переменные для формы редактирования
 let popupWindow = document.querySelector(".popup");
-let inputName = document.querySelector(".input-name");
-let inputDescription = document.querySelector(".input-description");
+let popupForm = document.querySelector('.popup__form');
+let inputName = document.querySelector(".popup__input_name");
+let inputDescription = document.querySelector(".popup__input_description");
 let profileTitle = document.querySelector(".profile__title");
 let profileSubtitle = document.querySelector(".profile__subtitle");
-let editButton = document.querySelector(".button-edit");
-let submitButton = document.querySelector(".button-save");
-let closePopupButton = document.querySelector(".button-close-popup");
+let editButton = document.querySelector(".profile__button-edit");
+let closePopupButton = document.querySelector(".popup__button-close");
 
 //Функция открытия popup
 function popupOpen() {
@@ -33,5 +33,14 @@ function handleFormSubmit(evt) {
 }
 
 editButton.addEventListener("click", popupOpen);
-submitButton.addEventListener("click", handleFormSubmit);
+popupForm.addEventListener("submit", handleFormSubmit);
 closePopupButton.addEventListener("click", popupClose);
+
+// Реализация нажатия/отжатия лайка на карточке
+let likeButtons = document.querySelectorAll('.card__button-like');
+
+for (let i = 0; i < likeButtons.length; i++) {
+    likeButtons[i].addEventListener('click', function() {
+      likeButtons[i].classList.toggle('card__button-like_active');
+    });
+  }
