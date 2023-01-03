1e1f82b558204068587277384b633962cef41f9a
Алексей <32929527+nub24@users.noreply.github.com>
2023-01-03 14:03:37 +0700

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,27 @@
 'use strict';
 
-// Реализация нажатия/отжатия лайка на карточке
-let likeButtons = document.querySelectorAll('.like-button');
-let likeButtonPath = './images/liked-button.svg';
-let unlikeButtonPath = './images/unliked-button.svg';
-
-for (let i = 0; i < likeButtons.length; i++) {
-  likeButtons[i].setAttribute('src', './images/unliked-button.svg');
-  likeButtons[i].addEventListener('click', function() {
-    if (likeButtons[i].getAttribute('src') === unlikeButtonPath) {
-      likeButtons[i].setAttribute('src', likeButtonPath);
-    } else {
-      likeButtons[i].setAttribute('src', unlikeButtonPath);
-    }
-  });
-}
-
-
 //Переменные для формы редактирования
 let popupWindow = document.querySelector('.popup');
-let inputName = document.querySelector('.popup__name');
-let inputDescription = document.querySelector('.popup__description');
+let inputName = document.querySelector('.input-name');
+let inputDescription = document.querySelector('.input-description');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let editButton = document.querySelector('.edit-button');
 let submitButton = document.querySelector('.save-button');
-let closePopupButton = document.querySelector('.popup__close-icon');
+let closePopupButton = document.querySelector('.popup__close-button');
 
-//Фкнкция открытия popup
+//Функция открытия popup
 function popupOpen() {
   //Значения инпутов при инициализвци popup-окна.
   inputName.value = profileTitle.textContent;
   inputDescription.value = profileSubtitle.textContent;
 
-  popupWindow.classList.replace('popup__unactive', 'popup__active');
+  popupWindow.classList.add('popup_active');
 }
 
 //Фкнкция закрытия popup
 function popupClose() {
-  popupWindow.classList.replace('popup__active', 'popup__unactive');
+  popupWindow.classList.remove('popup_active');
 }
 
 //Функция отправки формы
@@ -46,7 +29,7 @@ function handleFormSubmit (evt) {
   evt.preventDefault();
   profileTitle.textContent = inputName.value;
   profileSubtitle.textContent = inputDescription.value;
-  popupWindow.classList.replace('popup__active', 'popup__unactive');
+  popupClose();
 }
 
 editButton.addEventListener('click', popupOpen);
